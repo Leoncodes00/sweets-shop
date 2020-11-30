ba2f8dfa161d412ec433415a7a266fe0a97ad6ec
Leoncodes00 <chachaiyang3@gmail.com>
2020-11-30 01:00:23 -0600

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Menu, Sticky } from "semantic-ui-react";
+import "./App.css";
+import Shop from "./Shop/Shop";
+import About from "./About/About";
+import FAQ from "./FAQ/FAQ";
+import Contact from "./Contact/Contact";
+import ReactDOM from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      routes: "",
+    };
+  }
+  componentDidMount() {}
+
+  handleRoutes = (e, { name }) => {
+    this.setState({
+      routes: `${name}`,
+    });
+  };
+  render() {
+    return (
+      <div>
+        <Sticky>
+          <Menu
+            className="ui secondary menu"
+            style={{
+              fontSize: 25,
+              fontFamily: "Nerko One",
+              backgroundColor: "white",
+            }}
+          >
+            <Router>
+              <Menu.Item>Sweets</Menu.Item>
+              <div className="ui centered container">
+                <Link to="">
+                  <Menu.Item name="" onClick={this.handleRoutes}>
+                    Shop
+                  </Menu.Item>
+                </Link>
+                <Link to="/about">
+                  <Menu.Item name="about" onClick={this.handleRoutes}>
+                    About
+                  </Menu.Item>
+                </Link>
+                <Link to="/faq">
+                  <Menu.Item name="faq" onClick={this.handleRoutes}>
+                    FAQ
+                  </Menu.Item>
+                </Link>
+                <Link to="/contact">
+                  <Menu.Item name="contact" onClick={this.handleRoutes}>
+                    Contact
+                  </Menu.Item>
+                </Link>
+              </div>
+              <div className="ui container">
+                <Link to="/cart">
+                  <img
+                    style={{ width: "75%", height: "75%" }}
+                    src="https://img.icons8.com/dusk/64/000000/shopping-cart.png"
+                  />
+                </Link>
+                <Link to="/account">Account</Link>
+              </div>
+            </Router>
+          </Menu>
+        </Sticky>
+        {this.state.routes === "" ? <Shop /> : false}
+        {this.state.routes === "about" ? <About /> : false}
+        {this.state.routes === "faq" ? <FAQ /> : false}
+        {this.state.routes === "contact" ? <Contact /> : false}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Shop/Shop.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { Grid, Image, Card } from "semantic-ui-react";
+
+const Shop = (props) => {
+  return (
+    <Grid relaxed columns={5}>
+      <Grid.Row>
+        <Grid.Column>
+          <Image src="https://images.unsplash.com/photo-1601493700625-9185417898cf?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjZ8fGNhbmR5fGVufDB8fDB8&auto=format&fit=crop&w=500&q=60" />
+        </Grid.Column>
+        <Grid.Column>
+          <Image src="https://images.unsplash.com/photo-1534705867302-2a41394d2a3b?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mjd8fGNhbmR5fGVufDB8fDB8&auto=format&fit=crop&w=500&q=60" />
+        </Grid.Column>
+        <Grid.Column>
+          <Image src="https://images.unsplash.com/photo-1513593854556-94df5c54a8d6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mjl8fGNhbmR5fGVufDB8fDB8&auto=format&fit=crop&w=500&q=60" />
+        </Grid.Column>
+        <Grid.Column>
+          <Image src="https://images.unsplash.com/photo-1588756164640-a9f10546e1b3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjN8fGNhbmR5fGVufDB8fDB8&auto=format&fit=crop&w=500&q=60" />
+        </Grid.Column>
+        <Grid.Column>
+          <Image src="https://images.unsplash.com/photo-1571506165871-ee72a35bc9d4?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8Y2FuZHl8ZW58MHx8MHw%3D&auto=format&fit=crop&w=500&q=60" />
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row columns={5}>
+        <Grid.Column>
+          <Image src="https://images.unsplash.com/photo-1601493700625-9185417898cf?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjZ8fGNhbmR5fGVufDB8fDB8&auto=format&fit=crop&w=500&q=60" />
+        </Grid.Column>
+        <Grid.Column>
+          <Image src="https://images.unsplash.com/photo-1534705867302-2a41394d2a3b?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mjd8fGNhbmR5fGVufDB8fDB8&auto=format&fit=crop&w=500&q=60" />
+        </Grid.Column>
+        <Grid.Column>
+          <Image src="https://images.unsplash.com/photo-1513593854556-94df5c54a8d6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mjl8fGNhbmR5fGVufDB8fDB8&auto=format&fit=crop&w=500&q=60" />
+        </Grid.Column>
+        <Grid.Column>
+          <Image src="https://images.unsplash.com/photo-1588756164640-a9f10546e1b3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjN8fGNhbmR5fGVufDB8fDB8&auto=format&fit=crop&w=500&q=60" />
+        </Grid.Column>
+        <Grid.Column>
+          <Image src="https://images.unsplash.com/photo-1571506165871-ee72a35bc9d4?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8Y2FuZHl8ZW58MHx8MHw%3D&auto=format&fit=crop&w=500&q=60" />
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row columns={5}>
+        <Grid.Column>
+          <Image src="https://images.unsplash.com/photo-1601493700625-9185417898cf?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjZ8fGNhbmR5fGVufDB8fDB8&auto=format&fit=crop&w=500&q=60" />
+        </Grid.Column>
+        <Grid.Column>
+          <Image src="https://images.unsplash.com/photo-1534705867302-2a41394d2a3b?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mjd8fGNhbmR5fGVufDB8fDB8&auto=format&fit=crop&w=500&q=60" />
+        </Grid.Column>
+        <Grid.Column>
+          <Image src="https://images.unsplash.com/photo-1513593854556-94df5c54a8d6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mjl8fGNhbmR5fGVufDB8fDB8&auto=format&fit=crop&w=500&q=60" />
+        </Grid.Column>
+        <Grid.Column>
+          <Image src="https://images.unsplash.com/photo-1588756164640-a9f10546e1b3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjN8fGNhbmR5fGVufDB8fDB8&auto=format&fit=crop&w=500&q=60" />
+        </Grid.Column>
+        <Grid.Column>
+          <Image src="https://images.unsplash.com/photo-1571506165871-ee72a35bc9d4?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8Y2FuZHl8ZW58MHx8MHw%3D&auto=format&fit=crop&w=500&q=60" />
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
+};
+
+export default Shop;
